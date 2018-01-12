e56b98814f435cdde161b98aae2e0b01a38bfaae
spkvn <kevinmeagher1@hotmail.com>
2018-01-12 21:46:32 +1100

    learning how vue-cli + webpack works

---
+++ l16/src/components/Message.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="box">
+		<p>
+			<slot></slot>
+		</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style>
+	.box{padding:10px;border:1px solid darkgray;margin-bottom: 1em;}
+</style>
